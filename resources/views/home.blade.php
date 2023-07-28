dedddafb600a32cf6ce42666ac1cee0327054ee0
Alinaket <take.olesja@gmail.com>
2023-07-29 02:32:44 +0300

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,283 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="nav">
+        <ul>
+            <li><a href="#">Про нас</a></li>
+            <li><a href="#">Гарантія</a></li>
+            <li><a href="#">Доставка</a></li>
+            <li><a href="#">Контакти</a></li>
+        </ul>
+        <div class="account">
+            <ul>
+                <li><a href="#">Особистий кабінет</a></li>
+                <li><a href="#">Бути добровольцем</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="title">
+        <h1>Сервіз <span>пошуку</span> рабів</h1>
+        <p>Беріть участь у конкурсі та дістаньте Галину Іванівну в подарунок</p>
+        <em>Взяти участь</em>
+    </div>
+    <div class="quest">
+        <div class="cardList">
+            <div class="card">
+                <div class="img">
+                    <img src="https://politics.princeton.edu/sites/default/files/styles/square/public/images/p-5.jpeg?h=87dbaab7&itok=ub6jAL5Q" alt="">
+                    <img src="https://politics.princeton.edu/sites/default/files/styles/square/public/images/p-5.jpeg?h=87dbaab7&itok=ub6jAL5Q" alt="">
+                    <img src="https://politics.princeton.edu/sites/default/files/styles/square/public/images/p-5.jpeg?h=87dbaab7&itok=ub6jAL5Q" alt="">
+                </div>
+                <div class="point">
+                    <h2>Чоловіки</h2>
+                    <em>5 232</em>
+                    <span><i class="fa-solid fa-chevron-right"></i></span>
+                </div>
+            </div>
+            <div class="card">
+                <div class="img">
+                    <img src="https://politics.princeton.edu/sites/default/files/styles/square/public/images/p-5.jpeg?h=87dbaab7&itok=ub6jAL5Q" alt="">
+                    <img src="https://politics.princeton.edu/sites/default/files/styles/square/public/images/p-5.jpeg?h=87dbaab7&itok=ub6jAL5Q" alt="">
+                    <img src="https://politics.princeton.edu/sites/default/files/styles/square/public/images/p-5.jpeg?h=87dbaab7&itok=ub6jAL5Q" alt="">
+                </div>
+                <div class="point">
+                    <h2>Жінки</h2>
+                    <em>9 453</em>
+                    <span><i class="fa-solid fa-chevron-right"></i></span>
+                </div>
+            </div>
+            <div class="card">
+                <div class="img">
+                    <img src="https://politics.princeton.edu/sites/default/files/styles/square/public/images/p-5.jpeg?h=87dbaab7&itok=ub6jAL5Q" alt="">
+                    <img src="https://politics.princeton.edu/sites/default/files/styles/square/public/images/p-5.jpeg?h=87dbaab7&itok=ub6jAL5Q" alt="">
+                    <img src="https://politics.princeton.edu/sites/default/files/styles/square/public/images/p-5.jpeg?h=87dbaab7&itok=ub6jAL5Q" alt="">
+                </div>
+                <div class="point">
+                    <h2>Всі анкети</h2>
+                    <em>14 685</em>
+                    <span><i class="fa-solid fa-chevron-right"></i></span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="companies">
+        <div class="banner">
+            <div class="box">
+                <p>Компанії та користувачі</p>
+                <h3></h3>
+            </div>
+            <p><span>+40</span>Добровольців</p>
+            <p><span>+14000</span>Анкет рабів</p>
+            <button>Приєднатися</button>
+        </div>
+       <div class="img">
+           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8YUIux8I0CnySBmiI4jKYXiGxkEeOZgwHbILIbR_o5ce_SoUEZp599TadaWMgo9gtLQA&usqp=CAU" alt="">
+           <em>Обробили <span>365</span> анкет</em>
+       </div>
+        <div class="add">
+            <div class="img">
+                <img src="https://img.freepik.com/free-photo/portrait-happy-woman-with-digital-tablet_329181-11681.jpg?w=2000" alt="">
+            </div>
+            <div class="text">
+                <h3>Больше года работаем с TopModels и могу рекомендовать всем агенствам</h3>
+                <p><span>Наталія</span> Начальниця агенства </p>
+            </div>
+        </div>
+        <div class="box">
+            <div class="hr"></div>
+            <span><i class="fa-solid fa-arrow-right"></i></span>
+        </div>
+    </div>
+    <div class="catalog">
+        <div class="title">
+            <h2>Каталог</h2>
+            <h3>Самая велика база анкет</h3>
+        </div>
+        <div class="filter">
+            <div class="container">
+                <ul>
+                    <li>Топові</li>
+                    <li>Новинки</li>
+                    <li>Горячі раби</li>
+                </ul>
+                <div class="button">
+                    <button>Всі 2466 анкет</button>
+                </div>
+            </div>
+        </div>
+        <div class="cardList">
+            <div class="card">
+                <div class="img">
+                    <img src="https://www.newvisiontheatres.com/wp-content/uploads/2023/01/Yael-Shelbia.jpg" alt="">
+                    <em>75 фото</em>
+                </div>
+                <div class="info_people">
+                    <h3>Анастасія</h3>
+                    <em>15 років</em>
+                    <div class="other">
+                        <p>Подробиці</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="img">
+                    <img src="https://www.newvisiontheatres.com/wp-content/uploads/2023/01/Yael-Shelbia.jpg" alt="">
+                    <em>75 фото</em>
+                </div>
+                <div class="info_people">
+                    <h3>Анастасія</h3>
+                    <em>15 років</em>
+                    <div class="other">
+                        <p>Подробиці</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="img">
+                    <img src="https://www.newvisiontheatres.com/wp-content/uploads/2023/01/Yael-Shelbia.jpg" alt="">
+                    <em>75 фото</em>
+                </div>
+                <div class="info_people">
+                    <h3>Анастасія</h3>
+                    <em>15 років</em>
+                    <div class="other">
+                        <p>Подробиці</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="img">
+                    <img src="https://www.newvisiontheatres.com/wp-content/uploads/2023/01/Yael-Shelbia.jpg" alt="">
+                    <em>75 фото</em>
+                </div>
+                <div class="info_people">
+                    <h3>Анастасія</h3>
+                    <em>15 років</em>
+                    <div class="other">
+                        <p>Подробиці</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="img">
+                    <img src="https://www.newvisiontheatres.com/wp-content/uploads/2023/01/Yael-Shelbia.jpg" alt="">
+                    <em>75 фото</em>
+                </div>
+                <div class="info_people">
+                    <h3>Анастасія</h3>
+                    <em>15 років</em>
+                    <div class="other">
+                        <p>Подробиці</p>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="img">
+                    <img src="https://www.newvisiontheatres.com/wp-content/uploads/2023/01/Yael-Shelbia.jpg" alt="">
+                    <em>75 фото</em>
+                </div>
+                <div class="info_people">
+                    <h3>Анастасія</h3>
+                    <em>15 років</em>
+                    <div class="other">
+                        <p>Подробиці</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="button">
+            <button>Показати ще</button>
+        </div>
+    </div>
+    <div class="info">
+        <div class="safe_transaction">
+            <h3>безопасна сділка для рабовласників</h3>
+            <p>За весь цей час компанія вже зекономила, а также створила цілу екосистему рабів, среднього и крупного бізнеса</p>
+        </div>
+        <div class="rivalry">
+            <h3>Прозрачна конкуренція</h3>
+            <p>За цей час компанія вже заощадила десятки мільярдів бюджетних коштів, а також створила цілу
+                екосистему малого, середнього та великого бізнесу, орієнтованого на співпрацю насамперед із держструктурами.</p>
+        </div>
+    </div>
+    <div class="news">
+        <div class="title">
+            <h2>Новини</h2>
+            <h3>Слідкуємо за актуальним ринка</h3>
+        </div>
+        <div class="cardList">
+            <div class="card">
+                <div class="img">
+                    <img src="https://images.summitmedia-digital.com/preview/images/2023/02/27/abby-choi-10-things-nm.jpg" alt="">
+                </div>
+                <div class="text">
+                    <div class="data">
+                        <span>14 липня 2022</span>
+                        <p>Липень. 13 речей від українських брендів, на які варто звернути увагуЛипень.</p>
+                    </div>
+                </div>
+                <div class="arrow">
+                    <span><i class="fa-solid fa-arrow-right"></i></span>
+                </div>
+            </div>
+            <div class="card">
+                <div class="img">
+                    <img src="https://images.summitmedia-digital.com/preview/images/2023/02/27/abby-choi-10-things-nm.jpg" alt="">
+                </div>
+                <div class="text">
+                    <div class="data">
+                        <span>14 липня 2022</span>
+                        <p>Липень. 13 речей від українських брендів, на які варто звернути увагуЛипень.</p>
+                    </div>
+                </div>
+                <div class="arrow">
+                    <span><i class="fa-solid fa-arrow-right"></i></span>
+                </div>
+            </div>
+            <div class="card">
+                <div class="img">
+                    <img src="https://images.summitmedia-digital.com/preview/images/2023/02/27/abby-choi-10-things-nm.jpg" alt="">
+                </div>
+                <div class="text">
+                    <div class="data">
+                        <span>14 липня 2022</span>
+                        <p>Липень. 13 речей від українських брендів, на які варто звернути увагуЛипень.</p>
+                    </div>
+                </div>
+                <div class="arrow">
+                    <span><i class="fa-solid fa-arrow-right"></i></span>
+                </div>
+            </div>
+        </div>
+        <div class="button">
+            <button>Більше новин</button>
+        </div>
+    </div>
+    <div class="add_tg">
+        <h2>Ми дамо відповіді на найскладніші питання в нашому телеграмі</h2>
+    </div>
+    <div class="footer">
+        <div class="container">
+            <p>TOP РАБ</p>
+            <ul>
+                <li>Про нас</li>
+                <li>Майбутнім рабам</li>
+                <li>Рабовласникам</li>
+                <li>Контакти</li>
+            </ul>
+        </div>
+        <div class="social_network">
+            <span><i class="fa-brands fa-telegram"></i></span>
+            <span><i class="fa-brands fa-facebook"></i></span>
+            <span><i class="fa-brands fa-whatsapp"></i></span>
+        </div>
+    </div>
+</body>
+</html>
